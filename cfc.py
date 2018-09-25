3a73bbda204e51fe525347bec4e920282f94ed29
kariminf <kariminfo0@gmail.com>
2018-09-25 15:03:02 +0100

    a testing version

---
+++ cfc.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# list des composantes fortement connexes
+composants = []
+# ensemble des noeuds marqués plus
+plus = set()
+# ensemble des noeuds marqués moins
+moins = set()
+# ensemble des noeuds qui ne sont pas encore affectés à une composante
+noeuds_restants = set()
+
+# marquer les successeurs avec +
+def marquer_successeurs(v, graph):
+    global plus
+    succ = graph[v] # récupérer la list des successeurs de ce noeud
+    for u in succ: # parcourir les successeurs de v
+        if not u in plus: # si un successeur u n'est pas marqué +
+            plus.add(u) #marquer u avec +
+            marquer_successeurs(u, graph) # marquer les successeurs de u avec +
+
+# marquer les predecesseurs avec -
+def marquer_predecesseurs(v, graph):
+    global moins
+    for u in graph: #parcourir les éléments u du graph
+        succ = graph[u] #récupérer les successeurs de chaque élément
+        if (v in succ) and (u not in moins): # si v est un successeur de u et u n'est pas marqué -
+            moins.add(u) # marquer u avec -
+            marquer_predecesseurs(u, graph) # marquer les predecesseurs de u avec -
+
+def composantes_connexes(graph):
+    #Global: c'est pour accéder aux variables globales
+    #Python considère tous qui est dans une fonction comme variable locale
+    global composants
+    global plus
+    global moins
+    global noeuds_restants
+    # initialiser les listes
+    composants = []
+    plus = set()
+    moins = set()
+    # récupérer les noeuds à partir du graph
+    noeuds_restants = set(graph.keys())
+
+    while len(noeuds_restants) > 0 : # s'il existe des noeads restants (non affectés)
+        v = list(noeuds_restants)[0] # récupérer un des noeuds restants
+        plus.add(v) # marquer v avec +
+        moins.add(v) # marquer v avec -
+        marquer_successeurs(v, graph) # marqer ses successeurs avec +
+        marquer_predecesseurs(v, graph) #marquer ses predecesseurs avec -
+        inter = plus & moins # ensemble des noeuds marqués + et -
+        composants.append(list(inter)) # ajouter cet ensemble comme  composante connexe
+        noeuds_restants =  noeuds_restants - inter # supprimer ses éléments de la liste des noeuds restants
+        plus = set() # vider la list des +
+        moins = set() # vider la list des -
+
+    return composants # retourner les composantes connexes
+
+
+test = composantes_connexes({1:[2],2:[1,5],3:[4],4:[3,5],5:[6],6:[7],7:[8],8:[6,9],9:[]})
+
+print(test)
